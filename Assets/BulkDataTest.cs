622c1c4f4cbac9ee12029b70e20280aae6c6c7a4
Keijiro Takahashi <keijiro@gmail.com>
2024-06-13 22:12:36 +0900

    LockBuffer test

--- Assets/BulkDataTest.cs
+++ Assets/BulkDataTest.cs
@@ -1,36 +1,53 @@
 using UnityEngine;
+using System.Linq;
+using Array = System.Array;
 
 public class BulkDataTest : MonoBehaviour
 {
-    [SerializeField] int _bufferCount = 10;
-    [SerializeField] int _bufferSize = 1920 * 1080;
-    [SerializeField] bool _doGetData = true;
-    [SerializeField] bool _doSetData = true;
+    public enum AccessorType { SetData, LockBuffer }
 
-    ComputeBuffer[] _buffers;
-    int[] _data;
+    [field:SerializeField] public int BufferCount = 32;
+    [field:SerializeField] public int BufferSize = 1920 * 1080;
+    [field:SerializeField] public AccessorType Accessor;
 
-    void OnEnable()
+    GraphicsBuffer[] _buffers;
+    uint[] _data;
+
+    GraphicsBuffer NewBuffer()
+      => new GraphicsBuffer(GraphicsBuffer.Target.Structured,
+                            GraphicsBuffer.UsageFlags.LockBufferForWrite,
+                            BufferSize, sizeof(uint));
+
+    void RunSetData(GraphicsBuffer buffer)
+      => buffer.SetData(_data);
+
+    void RunLockBuffer(GraphicsBuffer buffer)
     {
-        _buffers = new ComputeBuffer[_bufferCount];
-
-        for (var i = 0; i < _bufferCount; i++)
-            _buffers[i] = new ComputeBuffer(_bufferSize, sizeof(int));
-
-        _data = new int[_bufferSize];
+        var array = buffer.LockBufferForWrite<uint>(0, BufferSize);
+        array.CopyFrom(_data);
+        buffer.UnlockBufferAfterWrite<uint>(BufferSize);
     }
 
-    void OnDisable()
+    void Start()
     {
-        foreach (var buffer in _buffers) buffer.Dispose();
+        _buffers = Enumerable.Range(0, BufferCount).
+                   Select(i => NewBuffer()).ToArray();
+        _data = new uint[BufferSize];
     }
+
+    void OnDestroy()
+      => Array.ForEach(_buffers, x => x.Dispose());
 
     void Update()
     {
         var sw = new System.Diagnostics.Stopwatch();
         sw.Start();
-        if (_doGetData) foreach (var buffer in _buffers) buffer.GetData(_data);
-        if (_doSetData) foreach (var buffer in _buffers) buffer.SetData(_data);
+
+        if (Accessor == AccessorType.SetData)
+            Array.ForEach(_buffers, x => RunSetData(x));
+        else
+            Array.ForEach(_buffers, x => RunLockBuffer(x));
+
         sw.Stop();
         Debug.Log($"{sw.Elapsed.TotalMilliseconds} ms");
     }
